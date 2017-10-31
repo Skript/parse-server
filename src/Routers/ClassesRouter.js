fec7202acfc34a249cf8a219ca6749d0d9d010e0
lukyanov <y.snaky@gmail.com>
2017-10-31 16:56:29 +0400

    lang=ru by default for Story queries

--- src/Routers/ClassesRouter.js
+++ src/Routers/ClassesRouter.js
@@ -25,6 +25,9 @@ export class ClassesRouter extends PromiseRouter {
     if (typeof body.where === 'string') {
       body.where = JSON.parse(body.where);
     }
+    if (this.className(req) === "Story" && !body.where.lang) {
+        body.where.lang = "ru";
+    }
     return rest.find(req.config, req.auth, this.className(req), body.where, options, req.info.clientSDK)
       .then((response) => {
         if (response && response.results) {
